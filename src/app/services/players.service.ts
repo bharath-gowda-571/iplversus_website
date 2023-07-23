1bd6c9f99eed1a3c4583bbafb13106c71da735a9
Bharath Gowda <bharath1234gowda@gmail.com>
2023-07-23 19:53:23 +0530

    changed the api endpoint and added actions

--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -3,46 +3,53 @@ import { Injectable } from '@angular/core';
 
 import { PlayerPopName,Runs,Extras,Wicket,Ball,BowlingInnings ,batVTeam } from '../player_pop_names';
 
-
+import { environment } from '../../environments/environment';
 
 import { Observable } from 'rxjs';
-// import { stringify } from 'querystring';
-// import 'rxjs/add/operator/toPromise'
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlayersService {
-
-  constructor(private http:HttpClient) { }
+  public BASE_URL!: string ;
+  constructor(private http:HttpClient) {
+    if(environment.production==false){
+     this.BASE_URL="http://localhost:8000/"
+    }
+    else{
+      this.BASE_URL=" https://ohgjfvrv98.execute-api.ap-south-1.amazonaws.com/prod/"
+    }
+   }
 
   getPlayerPopNames():Observable<PlayerPopName[]>{
-    return this.http.get<PlayerPopName[]>("https://ipl-versus-default-rtdb.asia-southeast1.firebasedatabase.app/pop_player_names.json");
+    return this.http.get<PlayerPopName[]>(this.BASE_URL+"pop_player_names");
   }
 
   getTeamNames():Observable<String[]>{
-    return this.http.get<String[]>("https://ipl-versus-default-rtdb.asia-southeast1.firebasedatabase.app/team_names.json");
+    return this.http.get<String[]>(this.BASE_URL+"team_names");
   }
 
   
   async getBatsmanVsBowlerData(batsman:string,bowler:string):Promise<Ball[]>
   {
-    return (await this.http.get<Ball[]>("https://ipl-versus-default-rtdb.asia-southeast1.firebasedatabase.app/batsman_vs_bowler_data/" + encodeURI(batsman) + "/" + encodeURI(bowler) + ".json").toPromise())
+    
+    return (await this.http.get<Ball[]>(this.BASE_URL+"batsman_vs_bowler_data/" + encodeURI(batsman) + "/" + encodeURI(bowler)).toPromise())
     
   }
 
   async getBatsmanVsTeamData(batsman:string,team:string):Promise<batVTeam[]>
   {
-    return (await this.http.get<batVTeam[]>("https://ipl-versus-default-rtdb.asia-southeast1.firebasedatabase.app/player_vs_team/" + encodeURI(batsman) + "/batting/" + encodeURI(team) + ".json").toPromise());
+    return (await this.http.get<batVTeam[]>(this.BASE_URL+"player_vs_team/" + encodeURI(batsman) + "/batting/" + encodeURI(team) ).toPromise());
   }
 
   async getBowlerVsTeamData(bowler:string,team:string):Promise<BowlingInnings[]>{
     
-    return (await this.http.get<BowlingInnings[]>("https://ipl-versus-default-rtdb.asia-southeast1.firebasedatabase.app/player_vs_team/"+encodeURI(bowler)+"/"+"bowling/"+encodeURI(team)+".json").toPromise())
+    return (await this.http.get<BowlingInnings[]>(this.BASE_URL+"player_vs_team/"+encodeURI(bowler)+"/"+"bowling/"+encodeURI(team)).toPromise())
 
   }
 
   async getPlayerPicture(name:string):Promise<string>{
-    return (await this.http.get<string>("https://ipl-versus-default-rtdb.asia-southeast1.firebasedatabase.app/player_pictures/"+encodeURI(name)+".json").toPromise())
+    return (await this.http.get<string>(this.BASE_URL+"player_pictures/"+encodeURI(name)).toPromise())
   }
 }
 
